03b857a56bf5a6e144465e89b4000e532e95a146
eolsonp <eolsonp@gmail.com>
2018-12-05 00:10:05 -0600

    win text

--- Roll-a-ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -26,6 +34,16 @@ public class PlayerController : MonoBehaviour {
        if(other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
